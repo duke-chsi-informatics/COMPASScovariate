38287fc4f4353e1d8bce461d2a3a386b61152878
Tyler Schappe <ts415@dcc-chsi-02.rc.duke.edu>
2022-12-19 09:39:30 -0500

    Set iters and reps in run_sim_cov.sh and pass to simu_cov.Rmd

--- incubator/run_sim_cov.sh
+++ incubator/run_sim_cov.sh
@@ -9,4 +9,7 @@
 export SINGULARITY_IMAGE="oras://gitlab-registry.oit.duke.edu/chsi-informatics/containers/compasscovariate-singularity:v003"
 export BIND_ARGS="--bind /work:/work"
 
+export ITER=1000
+export REP=1
+
 singularity exec ${BIND_ARGS} ${SINGULARITY_IMAGE} Rscript -e "rmarkdown::render('$HOME/project_repos/COMPASScovariate/incubator/simu_cov.Rmd')"
