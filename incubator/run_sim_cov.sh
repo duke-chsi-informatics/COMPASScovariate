21f3d7f9ee3b6bf28ee812e6e517ce4c0dcb9deb
Tyler Schappe <ts415@dcc-chsi-02.rc.duke.edu>
2022-12-19 10:48:26 -0500

    Fix import iter, reps from sbatch; add save fit object for each run; add I=15; increase iters and rep for full run.

--- incubator/run_sim_cov.sh
+++ incubator/run_sim_cov.sh
@@ -3,13 +3,13 @@
 #SBATCH -p chsi
 #SBATCH -A chsi
 #SBATCH --time=08:00:00
-#SBATCH --mem=1G
-#SBATCH -c 4
+#SBATCH --mem=8G
+#SBATCH -c 5
 
 export SINGULARITY_IMAGE="oras://gitlab-registry.oit.duke.edu/chsi-informatics/containers/compasscovariate-singularity:v003"
 export BIND_ARGS="--bind /work:/work"
 
-export ITER=1000
-export REP=1
+export ITER=100000
+export REP=5
 
 singularity exec ${BIND_ARGS} ${SINGULARITY_IMAGE} Rscript -e "rmarkdown::render('$HOME/project_repos/COMPASScovariate/incubator/simu_cov.Rmd')"
