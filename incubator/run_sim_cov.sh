472a0217fe768027764b0e692049f2f4140a1010
Tyler Schappe <ts415@dcc-chsi-02.rc.duke.edu>
2022-12-19 09:19:25 -0500

    Add sbatch script to run simu_cov.Rmd via SLURM using Singularity container

---
+++ incubator/run_sim_cov.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+#SBATCH -p chsi
+#SBATCH -A chsi
+#SBATCH --time=08:00:00
+#SBATCH --mem=1G
+#SBATCH -c 4
+
+export SINGULARITY_IMAGE="oras://gitlab-registry.oit.duke.edu/chsi-informatics/containers/compasscovariate-singularity:v003"
+export BIND_ARGS="--bind /work:/work"
+
+singularity exec ${BIND_ARGS} ${SINGULARITY_IMAGE} Rscript -e "rmarkdown::render('$HOME/project_repos/COMPASScovariate/incubator/simu_cov.Rmd')"
