2898813c99f2a711956d5dba5479124df956120f
Tyler Schappe <ts415@dcc-chsi-02.rc.duke.edu>
2022-12-20 09:28:41 -0500

    Move sim_rep to run_sim_cov.sh, add copy knit HTML to /work

--- incubator/run_sim_cov.sh
+++ incubator/run_sim_cov.sh
@@ -6,10 +6,17 @@
 #SBATCH --mem-per-cpu=1G
 #SBATCH -c 25
 
+#Singularity container setup
 export SINGULARITY_IMAGE="oras://gitlab-registry.oit.duke.edu/chsi-informatics/containers/compasscovariate-singularity:v003"
 export BIND_ARGS="--bind /work:/work"
 
-export ITER=100000
-export REP=5
+#Simulation arguments
+export ITER=10000
+export REP=2
+export SIM_REP=5
 
+#Run the simulation
 singularity exec ${BIND_ARGS} ${SINGULARITY_IMAGE} Rscript -e "rmarkdown::render('$HOME/project_repos/COMPASScovariate/incubator/simu_cov.Rmd')"
+
+#Copy the knit HTML to /work
+cp "${HOME}/project_repos/COMPASScovariate/incubator/simu_cov.html" "/work/${USER}/COMPASScovariate-sim/output"
