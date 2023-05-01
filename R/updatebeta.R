c79744c27bf1635f97b22d92b04e951974c10ebe
fwh6 <fwh6@gitlab.oit.duke.edu>
2023-05-01 16:56:22 -0400

    Originally, number of patients I wasn't defined within updatebeta.R

--- R/updatebeta.R
+++ R/updatebeta.R
@@ -1,6 +1,7 @@
 updatebeta = function(gammak, X1, nu, u, lambda2, tau2, W, K1,p1, sig2k){
   ### generate beta from MN
   beta_new = array(0, dim=c(K1,p1))
+  I = nrow(gammak) # originally missing
   p = p1-1
   K = K1+1
   for (k in 1:K1){
@@ -8,12 +9,12 @@ updatebeta = function(gammak, X1, nu, u, lambda2, tau2, W, K1,p1, sig2k){
     Bk_inv = diag(1/c(sig2k[k], lambda2*tau2[k]))
     tmp = t(X1)%*%Omegak%*%X1 + Bk_inv
     cm = chol(tmp)
-    Vk = Matrix::chol2inv(cm) # posterior variance matrix 
+    Vk = Matrix::chol2inv(cm) # posterior variance matrix
     kappak = gammak[,k] - 0.5
     mk = Vk%*%(t(X1)%*%kappak)
     beta_new[k,] = mvtnorm::rmvnorm(1, mk, Vk)
   }
-  
+
   ### updating the hyperparameters
   beta2 = 0.5*(beta_new[,-1])^2
   ## lambda2
@@ -31,6 +32,6 @@ updatebeta = function(gammak, X1, nu, u, lambda2, tau2, W, K1,p1, sig2k){
   u = invgamma::rinvgamma(K1, shape = 1, rate = 1+1/tau2)
   ## W
   W = matrix(sapply(as.vector(X1%*%t(beta_new)), function(x) pgdraw::pgdraw(1,x)), nrow = I)
-  
+
   return(list("W" = W, "u" = u, "nu" = nu, "tau2" = tau2, "lambda2" = lambda2, "beta" = beta_new ))
 }
